fc2fc65a4e27f2fd0b89f68334ce81927b7a74af
GinRay8 <136614240+GinRay8@users.noreply.github.com>
2023-11-11 22:58:16 +0200

    add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,48 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+let modalInstance = null;
+
+function galleryMarkup(items) {
+    return items.map((item) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img
+              class="gallery__image"
+              src="${item.preview}"
+              data-source="${item.original}"
+              alt="${item.description}"
+            />
+        </a>
+    </li>`
+    ).join("");
+};
+
+const addGalleryMarkup = galleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML("afterbegin", addGalleryMarkup);
+
+gallery.addEventListener('click', openImage);
+
+function openImage(event) {
+    event.preventDefault()
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+    modalInstance = instance;
+    instance.show()
+    document.addEventListener('keydown', closeModalOnEscape);
+};
+
+function closeModalOnEscape(event) {
+    if (event.code === 'Escape') {
+        modalInstance.close();
+        document.removeEventListener('keydown', closeModalOnEscape);
+    }
+}
